ccda5d8effc6a93d4d737180111653307bb3e331
Eric Jones <eaj010@email.uark.edu>
2018-01-24 18:26:28 -0600

    Renamed server class and implemented OOP
    Calling the Server class SocketServer.java was confusing me since the java object it uses is java.net.ServerSocket. Renamed to CommandServer.java.
    I also setup an OOP style of functionality instead of the static method I used for proof of concept.

---
+++ src/CommandServer.java
@@ -0,0 +1,58 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+class CommandServer{
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+    
+    public CommandServer(int portNum) throws IOException{
+        this.serverSocket = new ServerSocket(portNum);
+        this.clientSocket = serverSocket.accept();
+        this.out = new PrintWriter(clientSocket.getOutputStream(), true);
+        this.in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); 
+    }
+   
+    public void run(){
+        try{  
+            while(true){
+                String input = in.readLine();
+                if(input != null){
+                    if(input.equals("Go")){
+                        //Do stuff
+                        System.out.println("Process Images");
+                        out.println("Done");
+                    }
+                    else if(input.equals("End"))
+                        break;
+                    else
+                        System.out.println(input+ " Was an unknown message");
+                }
+            }
+        }
+        catch(IOException e){
+            System.err.println("ERROR READING COMMAND FROM PORT");
+            System.err.println(e.getMessage());
+        }
+    }
+    
+     public static void main(String[] args){
+        CommandServer cmdServer;
+        try{
+            cmdServer = new CommandServer(4444);
+            cmdServer.run();
+        }
+        catch(IOException e){
+            System.err.println("ERROR CREATING COMMAND SERVER");
+            System.err.println(e.getMessage());
+        }
+        
+        
+    }
+    
+}
